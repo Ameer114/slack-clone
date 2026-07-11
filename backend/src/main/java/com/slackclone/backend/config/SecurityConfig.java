caae9135a2ad81b7eeb7f892e1c3eac333d6c60c
Ameer114 <ameernagarasi6@gmail.com>
2026-07-11 16:14:33 +0530

    fixed config file

--- backend/src/main/java/com/slackclone/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/slackclone/backend/config/SecurityConfig.java
@@ -4,7 +4,11 @@ import com.slackclone.backend.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.Customizer;
 
@@ -58,5 +62,29 @@ public class SecurityConfig {
 //                .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:5173",
+                "https://*.onrender.com"
+        ));
+
+        configuration.setAllowedMethods(Arrays.asList("*"));
+
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
